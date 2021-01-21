dd882de51c41e123ad2cedaaa5e4662645f732ac
ggyu777 <73706252+ggyu777@users.noreply.github.com>
2021-01-21 18:01:10 +0900

    이하동문

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import App from './App' // 요게 App.js파일을 뜻함
 import { colors } from './components/utils/_var'
 import { injectGlobal } from 'styled-components'
 import registerServiceWorker from './registerServiceWorker'
@@ -13,5 +13,5 @@ injectGlobal`
   }
 `
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root')) // 여기서 <App />가 App.js파일을 뜻함, root는 index.html에 있는 아이디 값
 registerServiceWorker()
